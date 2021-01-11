d75043857b9553728c0d89a467ad25554f86af1b
stern <stern@noemail.net>
2021-01-11 19:02:45 +0000

    Make login sleep for a certain amount of time. In the current setting, login will take at least 500 msecs. If the comparison of the given and stored credential will take sharter, zettelstore will put itself into sleep for the difference to 500 msecs. If the comparison takes longer, Zettelstore will slepp for approx 100 msecs.
    This is the first attempt to mitigate login attacks.
    
    FossilOrigin-Name: c008fe2d0bda3c338933550d5d1288f30dbe41bd48a4ac5c6d8f302b937f421e

--- auth/cred/cred.go
+++ auth/cred/cred.go
@@ -13,6 +13,7 @@ package cred
 
 import (
 	"bytes"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 	"zettelstore.de/z/domain/id"
@@ -20,24 +21,30 @@ import (
 
 // HashCredential returns a hashed vesion of the given credential
 func HashCredential(zid id.Zid, ident string, credential string) (string, error) {
-	res, err := bcrypt.GenerateFromPassword(
-		createFullCredential(zid, ident, credential), bcrypt.DefaultCost)
+	fullCredential := createFullCredential(zid, ident, credential)
+	res, err := bcrypt.GenerateFromPassword(fullCredential, bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
 	return string(res), nil
 }
 
-// CompareHashAndCredential checks, whether the hashedCredential is a possible
+const (
+	durMinWait = 500 * time.Millisecond // minimum wait time after login
+	minSleep   = 100 * time.Millisecond // minimum sleep time, even for slow credential check
+)
+
+// CompareHashAndCredential checks, whether the hashed credential is a possible
 // value when hashing the credential.
-func CompareHashAndCredential(
-	hashedCredential string,
-	zid id.Zid,
-	ident string,
-	credential string,
-) (bool, error) {
-	err := bcrypt.CompareHashAndPassword(
-		[]byte(hashedCredential), createFullCredential(zid, ident, credential))
+func CompareHashAndCredential(hashed string, zid id.Zid, ident string, credential string) (bool, error) {
+	fullCredential := createFullCredential(zid, ident, credential)
+	start := time.Now()
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), fullCredential)
+	if elapsed := time.Since(start); elapsed+minSleep < durMinWait {
+		time.Sleep(durMinWait - elapsed)
+	} else {
+		time.Sleep(minSleep)
+	}
 	if err == nil {
 		return true, nil
 	}
