a94e0b187656cf68cf2371ca3b022416e5ad738e
stern <ds@zettelstore.de>
2021-03-03 15:23:11 +0000

    Server: Runtime Values -> Runtime Metrics.
    Make use of runtime/metrics instead of runtime.GetMemStats (and
    runtime/debug.ReadGCStats).
    
    FossilOrigin-Name: 632912699ec429febf448f93e97b213c4f3a4c2c9f414b8f3d3b3e85648f78a4

--- place/progplace/runtime.go
+++ place/progplace/runtime.go
@@ -13,9 +13,8 @@ package progplace
 
 import (
 	"fmt"
-	"runtime"
+	"runtime/metrics"
 	"strings"
-	"time"
 
 	"zettelstore.de/z/domain/id"
 	"zettelstore.de/z/domain/meta"
@@ -26,33 +25,48 @@ func genRuntimeM(zid id.Zid) *meta.Meta {
 		return nil
 	}
 	m := meta.New(zid)
-	m.Set(meta.KeyTitle, "Zettelstore Runtime Values")
+	m.Set(meta.KeyTitle, "Zettelstore Runtime Metrics")
 	return m
 }
 
 func genRuntimeC(*meta.Meta) string {
+	var samples []metrics.Sample
+	all := metrics.All()
+	for _, d := range all {
+		if d.Kind == metrics.KindFloat64Histogram {
+			continue
+		}
+		samples = append(samples, metrics.Sample{Name: d.Name})
+	}
+	metrics.Read(samples)
+
 	var sb strings.Builder
 	sb.WriteString("|=Name|=Value>\n")
-	fmt.Fprintf(&sb, "|Number of CPUs|%v\n", runtime.NumCPU())
-	fmt.Fprintf(&sb, "|Number of goroutines|%v\n", runtime.NumGoroutine())
-	fmt.Fprintf(&sb, "|Number of Cgo calls|%v\n", runtime.NumCgoCall())
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	fmt.Fprintf(&sb, "|Memory from OS|%v\n", m.Sys)
-	fmt.Fprintf(&sb, "|Objects active|%v\n", m.Mallocs-m.Frees)
-	fmt.Fprintf(&sb, "|Heap alloc|%v\n", m.HeapAlloc)
-	fmt.Fprintf(&sb, "|Heap sys|%v\n", m.HeapSys)
-	fmt.Fprintf(&sb, "|Heap idle|%v\n", m.HeapIdle)
-	fmt.Fprintf(&sb, "|Heap in use|%v\n", m.HeapInuse)
-	fmt.Fprintf(&sb, "|Heap released|%v\n", m.HeapReleased)
-	fmt.Fprintf(&sb, "|Heap objects|%v\n", m.HeapObjects)
-	fmt.Fprintf(&sb, "|Stack in use|%v\n", m.StackInuse)
-	fmt.Fprintf(&sb, "|Stack sys|%v\n", m.StackSys)
-	fmt.Fprintf(&sb, "|Garbage collection metadata|%v\n", m.GCSys)
-	fmt.Fprintf(&sb, "|Last garbage collection|%v\n", time.Unix((int64)(m.LastGC/1000000000), 0))
-	fmt.Fprintf(&sb, "|Garbage collection goal|%v\n", m.NextGC)
-	fmt.Fprintf(&sb, "|Garbage collections|%v\n", m.NumGC)
-	fmt.Fprintf(&sb, "|Forced garbage collections|%v\n", m.NumForcedGC)
-	fmt.Fprintf(&sb, "|Garbage collection fraction|%.3f%%\n", m.GCCPUFraction*100.0)
+	i := 0
+	for _, d := range all {
+		if d.Kind == metrics.KindFloat64Histogram {
+			continue
+		}
+		descr := d.Description
+		if pos := strings.IndexByte(descr, '.'); pos > 0 {
+			descr = descr[:pos]
+		}
+		fmt.Fprintf(&sb, "|%s|", descr)
+		value := samples[i].Value
+		i++
+		switch value.Kind() {
+		case metrics.KindUint64:
+			fmt.Fprintf(&sb, "%v", value.Uint64())
+		case metrics.KindFloat64:
+			fmt.Fprintf(&sb, "%v", value.Float64())
+		case metrics.KindFloat64Histogram:
+			sb.WriteString("???")
+		case metrics.KindBad:
+			sb.WriteString("BAD")
+		default:
+			fmt.Fprintf(&sb, "(unexpected metric kind: %v)", value.Kind())
+		}
+		sb.WriteByte('\n')
+	}
 	return sb.String()
 }
