ed16825a44379b0eaafaa74c061f8f55f76c5a54
stern <ds@zettelstore.de>
2021-06-07 11:39:04 +0000

    Rewrite text encoder with ast.Walk
    FossilOrigin-Name: 1c854aad72502f8988720efd2e862c26466b7d1914d9a922634c29430b8a7a2a

--- encoder/textenc/textenc.go
+++ encoder/textenc/textenc.go
@@ -85,7 +85,7 @@ func (te *textEncoder) WriteBlocks(w io.Writer, bs ast.BlockSlice) (int, error) 
 // WriteInlines writes an inline slice to the writer
 func (te *textEncoder) WriteInlines(w io.Writer, is ast.InlineSlice) (int, error) {
 	v := newVisitor(w)
-	v.acceptInlineSlice(is)
+	ast.WalkInlineSlice(v, is)
 	length, err := v.b.Flush()
 	return length, err
 }
@@ -99,184 +99,114 @@ func newVisitor(w io.Writer) *visitor {
 	return &visitor{b: encoder.NewBufWriter(w)}
 }
 
-// VisitPara emits text code for a paragraph
-func (v *visitor) VisitPara(pn *ast.ParaNode) {
-	v.acceptInlineSlice(pn.Inlines)
-}
-
-// VisitVerbatim emits text for verbatim lines.
-func (v *visitor) VisitVerbatim(vn *ast.VerbatimNode) {
-	if vn.Code == ast.VerbatimComment {
-		return
-	}
-	for i, line := range vn.Lines {
-		if i > 0 {
-			v.b.WriteByte('\n')
+func (v *visitor) Visit(node ast.Node) ast.WalkVisitor {
+	switch n := node.(type) {
+	case *ast.VerbatimNode:
+		if n.Code == ast.VerbatimComment {
+			return nil
 		}
-		v.b.WriteString(line)
-	}
-}
-
-// VisitRegion writes text code for block regions.
-func (v *visitor) VisitRegion(rn *ast.RegionNode) {
-	v.acceptBlockSlice(rn.Blocks)
-	if len(rn.Inlines) > 0 {
-		v.b.WriteByte('\n')
-		v.acceptInlineSlice(rn.Inlines)
-	}
-}
-
-// VisitHeading writes the text code for a heading.
-func (v *visitor) VisitHeading(hn *ast.HeadingNode) {
-	v.acceptInlineSlice(hn.Inlines)
-}
-
-// VisitHRule writes nothing for a horizontal rule.
-func (v *visitor) VisitHRule(hn *ast.HRuleNode) {}
-
-// VisitNestedList writes text code for lists and blockquotes.
-func (v *visitor) VisitNestedList(ln *ast.NestedListNode) {
-	for i, item := range ln.Items {
-		if i > 0 {
-			v.b.WriteByte('\n')
-		}
-		v.acceptItemSlice(item)
-	}
-}
-
-// VisitDescriptionList emits a text for a description list.
-func (v *visitor) VisitDescriptionList(dn *ast.DescriptionListNode) {
-	for i, descr := range dn.Descriptions {
-		if i > 0 {
-			v.b.WriteByte('\n')
-		}
-		v.acceptInlineSlice(descr.Term)
-
-		for _, b := range descr.Descriptions {
-			v.b.WriteByte('\n')
-			v.acceptDescriptionSlice(b)
-		}
-	}
-}
-
-// VisitTable emits a text table.
-func (v *visitor) VisitTable(tn *ast.TableNode) {
-	if len(tn.Header) > 0 {
-		for i, cell := range tn.Header {
+		for i, line := range n.Lines {
 			if i > 0 {
-				v.b.WriteByte(' ')
+				v.b.WriteByte('\n')
 			}
-			v.acceptInlineSlice(cell.Inlines)
+			v.b.WriteString(line)
 		}
-		v.b.WriteByte('\n')
-	}
-	for i, row := range tn.Rows {
-		if i > 0 {
+		return nil
+	case *ast.RegionNode:
+		v.acceptBlockSlice(n.Blocks)
+		if len(n.Inlines) > 0 {
+			v.b.WriteByte('\n')
+			ast.WalkInlineSlice(v, n.Inlines)
+		}
+		return nil
+	case *ast.NestedListNode:
+		for i, item := range n.Items {
+			if i > 0 {
+				v.b.WriteByte('\n')
+			}
+			for j, it := range item {
+				if j > 0 {
+					v.b.WriteByte('\n')
+				}
+				ast.Walk(v, it)
+			}
+		}
+		return nil
+	case *ast.DescriptionListNode:
+		for i, descr := range n.Descriptions {
+			if i > 0 {
+				v.b.WriteByte('\n')
+			}
+			ast.WalkInlineSlice(v, descr.Term)
+			for _, b := range descr.Descriptions {
+				v.b.WriteByte('\n')
+				for k, d := range b {
+					if k > 0 {
+						v.b.WriteByte('\n')
+					}
+					ast.Walk(v, d)
+				}
+			}
+		}
+		return nil
+	case *ast.TableNode:
+		if len(n.Header) > 0 {
+			v.writeRow(n.Header)
 			v.b.WriteByte('\n')
 		}
-		for j, cell := range row {
-			if j > 0 {
-				v.b.WriteByte(' ')
+		for i, row := range n.Rows {
+			if i > 0 {
+				v.b.WriteByte('\n')
 			}
-			v.acceptInlineSlice(cell.Inlines)
+			v.writeRow(row)
+		}
+		return nil
+	case *ast.TextNode:
+		v.b.WriteString(n.Text)
+		return nil
+	case *ast.TagNode:
+		v.b.WriteStrings("#", n.Tag)
+		return nil
+	case *ast.SpaceNode:
+		v.b.WriteByte(' ')
+		return nil
+	case *ast.BreakNode:
+		if n.Hard {
+			v.b.WriteByte('\n')
+		} else {
+			v.b.WriteByte(' ')
+		}
+		return nil
+	case *ast.LinkNode:
+		if !n.OnlyRef {
+			ast.WalkInlineSlice(v, n.Inlines)
+		}
+		return nil
+	case *ast.FootnoteNode:
+		v.b.WriteByte(' ')
+		return v // No 'return nil' to write text
+	case *ast.LiteralNode:
+		if n.Code != ast.LiteralComment {
+			v.b.WriteString(n.Text)
 		}
 	}
+	return v
 }
 
-// VisitBLOB writes nothing, because it contains no text.
-func (v *visitor) VisitBLOB(bn *ast.BLOBNode) {}
-
-// VisitText writes text content.
-func (v *visitor) VisitText(tn *ast.TextNode) {
-	v.b.WriteString(tn.Text)
-}
-
-// VisitTag writes tag content.
-func (v *visitor) VisitTag(tn *ast.TagNode) {
-	v.b.WriteStrings("#", tn.Tag)
-}
-
-// VisitSpace emits a white space.
-func (v *visitor) VisitSpace(sn *ast.SpaceNode) {
-	v.b.WriteByte(' ')
-}
-
-// VisitBreak writes text code for line breaks.
-func (v *visitor) VisitBreak(bn *ast.BreakNode) {
-	if bn.Hard {
-		v.b.WriteByte('\n')
-	} else {
-		v.b.WriteByte(' ')
+func (v *visitor) writeRow(row ast.TableRow) {
+	for i, cell := range row {
+		if i > 0 {
+			v.b.WriteByte(' ')
+		}
+		ast.WalkInlineSlice(v, cell.Inlines)
 	}
 }
-
-// VisitLink writes text code for links.
-func (v *visitor) VisitLink(ln *ast.LinkNode) {
-	if !ln.OnlyRef {
-		v.acceptInlineSlice(ln.Inlines)
-	}
-}
-
-// VisitImage writes text code for images.
-func (v *visitor) VisitImage(in *ast.ImageNode) {
-	v.acceptInlineSlice(in.Inlines)
-}
-
-// VisitCite writes code for citations.
-func (v *visitor) VisitCite(cn *ast.CiteNode) {
-	v.acceptInlineSlice(cn.Inlines)
-}
-
-// VisitFootnote write text code for a footnote.
-func (v *visitor) VisitFootnote(fn *ast.FootnoteNode) {
-	v.b.WriteByte(' ')
-	v.acceptInlineSlice(fn.Inlines)
-}
-
-// VisitMark writes nothing for a mark.
-func (v *visitor) VisitMark(mn *ast.MarkNode) {}
-
-// VisitFormat write text code for formatting text.
-func (v *visitor) VisitFormat(fn *ast.FormatNode) {
-	v.acceptInlineSlice(fn.Inlines)
-}
-
-// VisitLiteral write text code for literal inline text.
-func (v *visitor) VisitLiteral(ln *ast.LiteralNode) {
-	if ln.Code != ast.LiteralComment {
-		v.b.WriteString(ln.Text)
-	}
-}
-
-// VisitAttributes never writes any attribute data.
-func (v *visitor) VisitAttributes(a *ast.Attributes) {}
 
 func (v *visitor) acceptBlockSlice(bns ast.BlockSlice) {
 	for i, bn := range bns {
 		if i > 0 {
 			v.b.WriteByte('\n')
 		}
-		bn.Accept(v)
-	}
-}
-func (v *visitor) acceptItemSlice(ins ast.ItemSlice) {
-	for i, in := range ins {
-		if i > 0 {
-			v.b.WriteByte('\n')
-		}
-		in.Accept(v)
-	}
-}
-func (v *visitor) acceptDescriptionSlice(dns ast.DescriptionSlice) {
-	for i, dn := range dns {
-		if i > 0 {
-			v.b.WriteByte('\n')
-		}
-		dn.Accept(v)
-	}
-}
-func (v *visitor) acceptInlineSlice(ins ast.InlineSlice) {
-	for _, in := range ins {
-		in.Accept(v)
+		ast.Walk(v, bn)
 	}
 }
