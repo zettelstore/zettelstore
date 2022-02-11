02f50996e4d935267f04c4279c277e0f47aa86a0
t73fde <>
2022-02-11 23:50:09 +0000

    Remove buffer in encoder/buffer.go
    FossilOrigin-Name: 5c652f3ff24879a1b46fb5a724a064193ffac9198e7f1822624ca0a9426f38e8

--- encoder/buffer.go
+++ encoder/buffer.go
@@ -1,15 +1,13 @@
 //-----------------------------------------------------------------------------
-// Copyright (c) 2020-2021 Detlef Stern
+// Copyright (c) 2020-2022 Detlef Stern
 //
-// This file is part of zettelstore.
+// This file is part of Zettelstore.
 //
 // Zettelstore is licensed under the latest version of the EUPL (European Union
 // Public License). Please see file LICENSE.txt for your rights and obligations
 // under this license.
 //-----------------------------------------------------------------------------
 
-// Package encoder provides a generic interface to encode the abstract syntax
-// tree into some text form.
 package encoder
 
 import (
@@ -21,28 +19,22 @@ import (
 type BufWriter struct {
 	w      io.Writer // The io.Writer to write to
 	err    error     // Collect error
-	length int       // Sum length
-	buf    []byte    // Buffer to collect bytes
+	length int       // Collected length
 }
 
 // NewBufWriter creates a new BufWriter
 func NewBufWriter(w io.Writer) BufWriter {
-	return BufWriter{w: w, buf: make([]byte, 0, 4096)}
+	return BufWriter{w: w}
 }
 
 // Write writes the contents of p into the buffer.
-func (w *BufWriter) Write(p []byte) (int, error) {
+func (w *BufWriter) Write(p []byte) (l int, err error) {
 	if w.err != nil {
 		return 0, w.err
 	}
-	w.buf = append(w.buf, p...)
-	if len(w.buf) > 2048 {
-		w.flush()
-		if w.err != nil {
-			return 0, w.err
-		}
-	}
-	return len(p), nil
+	l, w.err = w.w.Write(p)
+	w.length += l
+	return l, w.err
 }
 
 // WriteString writes the contents of s into the buffer.
@@ -50,10 +42,9 @@ func (w *BufWriter) WriteString(s string) {
 	if w.err != nil {
 		return
 	}
-	w.buf = append(w.buf, s...)
-	if len(w.buf) > 2048 {
-		w.flush()
-	}
+	var l int
+	l, w.err = io.WriteString(w.w, s)
+	w.length += l
 }
 
 // WriteStrings writes the contents of sl into the buffer.
@@ -65,45 +56,30 @@ func (w *BufWriter) WriteStrings(sl ...string) {
 
 // WriteByte writes the content of b into the buffer.
 func (w *BufWriter) WriteByte(b byte) error {
-	w.buf = append(w.buf, b)
-	return nil
+	var l int
+	l, err := w.Write([]byte{b})
+	w.length += l
+	return err
 }
 
 // WriteBytes writes the content of bs into the buffer.
 func (w *BufWriter) WriteBytes(bs ...byte) {
-	w.buf = append(w.buf, bs...)
+	w.Write(bs)
 }
 
 // WriteBase64 writes the content of p into the buffer, encoded with base64.
 func (w *BufWriter) WriteBase64(p []byte) {
 	if w.err == nil {
-		w.flush()
-	}
-	if w.err == nil {
 		encoder := base64.NewEncoder(base64.StdEncoding, w.w)
-		length, err := encoder.Write(p)
-		w.length += length
+		var l int
+		l, w.err = encoder.Write(p)
+		w.length += l
 		err1 := encoder.Close()
-		if err == nil {
+		if w.err == nil {
 			w.err = err1
-		} else {
-			w.err = err
 		}
 	}
 }
 
-// Flush writes any buffered data to the underlying io.Writer. It returns the
-// number of bytes written and an error if something went wrong.
-func (w *BufWriter) Flush() (int, error) {
-	if w.err == nil {
-		w.flush()
-	}
-	return w.length, w.err
-}
-
-func (w *BufWriter) flush() {
-	length, err := w.w.Write(w.buf)
-	w.buf = w.buf[:0]
-	w.length += length
-	w.err = err
-}
+// Flush returns the collected length and error.
+func (w *BufWriter) Flush() (int, error) { return w.length, w.err }
