0119fd3a38bc77ac85e50d8d0f97aabc12f4c247
stern <ds@zettelstore.de>
2022-09-16 10:28:33 +0000

    Add integration test by testing "naughty strings"
    FossilOrigin-Name: f6c70df39526df7160ecf84ea7750fb931453205c82ce34ad75a03798ee71976

---
+++ testdata/naughty/README.md
@@ -0,0 +1,6 @@
+# Big List of Naughty Strings
+
+A list of strings which have a high probability of causing issues when used as user-input data.
+
+* Source: https://github.com/minimaxir/big-list-of-naughty-strings
+* License: MIT, (c) 2015-2020 Max Woolf (see file LICENSE)
---
+++ tests/naughtystrings_test.go
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2020-2022 Detlef Stern
+//
+// This file is part of Zettelstore.
+//
+// Zettelstore is licensed under the latest version of the EUPL (European Union
+// Public License). Please see file LICENSE.txt for your rights and obligations
+// under this license.
+//-----------------------------------------------------------------------------
+
+package tests
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+
+	_ "zettelstore.de/z/cmd"
+	"zettelstore.de/z/domain/meta"
+	"zettelstore.de/z/encoder"
+	"zettelstore.de/z/input"
+	"zettelstore.de/z/parser"
+)
+
+// Test all parser / encoder with a list of "naughty strings", i.e. unusual strings
+// that often crash software.
+
+func getNaughtyStrings() (result []string, err error) {
+	fpath := filepath.Join("..", "testdata", "naughty", "blns.txt")
+	file, err := os.Open(fpath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if text := scanner.Text(); text != "" && text[0] != '#' {
+			result = append(result, text)
+		}
+	}
+	return result, scanner.Err()
+}
+
+func getAllParser() (result []*parser.Info) {
+	for _, pname := range parser.GetSyntaxes() {
+		pinfo := parser.Get(pname)
+		if pname == pinfo.Name {
+			result = append(result, pinfo)
+		}
+	}
+	return result
+}
+
+func getAllEncoder() (result []encoder.Encoder) {
+	for _, enc := range encoder.GetEncodings() {
+		e := encoder.Create(enc)
+		result = append(result, e)
+	}
+	return result
+}
+
+func TestNaughtyStringParser(t *testing.T) {
+	blns, err := getNaughtyStrings()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(blns) == 0 {
+		t.Fatal("no naughty strings found")
+	}
+	pinfos := getAllParser()
+	if len(pinfos) == 0 {
+		t.Fatal("no parser found")
+	}
+	encs := getAllEncoder()
+	if len(encs) == 0 {
+		t.Fatal("no encoder found")
+	}
+	for _, s := range blns {
+		for _, pinfo := range pinfos {
+			bs := pinfo.ParseBlocks(input.NewInput([]byte(s)), &meta.Meta{}, pinfo.Name)
+			is := pinfo.ParseInlines(input.NewInput([]byte(s)), pinfo.Name)
+			for _, enc := range encs {
+				_, err = enc.WriteBlocks(io.Discard, &bs)
+				if err != nil {
+					t.Error(err)
+				}
+				_, err = enc.WriteInlines(io.Discard, &is)
+				if err != nil {
+					t.Error(err)
+				}
+			}
+		}
+	}
+}
