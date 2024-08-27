6837b55fcbd245173b35500b132ba4bcb4d27082
stern <ds@zettelstore.de>
2024-08-27 12:36:59 +0000

    Fix css error for tables in info view
    FossilOrigin-Name: 56f4be0ca22202acb913f0b47055eb8745a6603e36ced8a5dfab82d290100d43

--- box/constbox/constbox.go
+++ box/constbox/constbox.go
@@ -280,7 +280,7 @@ var constZettelMap = map[id.Zid]constZettel{
 			api.KeyRole:       api.ValueRoleConfiguration,
 			api.KeySyntax:     meta.SyntaxCSS,
 			api.KeyCreated:    "20200804111624",
-			api.KeyModified:   "20240826142300",
+			api.KeyModified:   "20240827143500",
 			api.KeyVisibility: api.ValueVisibilityPublic,
 		},
 		zettel.NewContent(contentBaseCSS)},
